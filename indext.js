94b38129e2ebd2894827bff176302200fb729f7a
Angelaacholdeng <79209806+Angelaacholdeng@users.noreply.github.com>
2021-03-21 14:15:40 +0300

    Update indext.js

--- indext.js
+++ indext.js
@@ -3,3 +3,14 @@ function money(fruitName,quantity){
     return quantity+" "+fruitName+" for KES "+total;
 }console.log (money("Oranges",2));
 money();
+/*var car ={
+  brand:"brand",
+   model:"1000",
+   color:"black",
+ }    // example of object
+
+ function  car(){
+    var  brand="prado"
+    var  color="black"
+ }
+car();//Example of function */
